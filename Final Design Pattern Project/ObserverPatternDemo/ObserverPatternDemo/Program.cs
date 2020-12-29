58915ff0d66ed0a70ca326e7cf8a068b52ae7e50
Nham Do Hai Ninh <71186825+tswizzle141@users.noreply.github.com>
2020-12-29 16:16:41 +0700

    Add files via upload

---
+++ Final Design Pattern Project/ObserverPatternDemo/ObserverPatternDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ObserverPatternDemo{
+    public class Program{
+        public static void Main(){
+            Subject subject = new Subject();
+
+            new HexaObserver(subject);
+            new OctalObserver(subject);
+            new BinaryObserver(subject);
+
+            Console.WriteLine("First state change: 15"); subject.setState(15);
+            Console.WriteLine("Second state change: 10"); subject.setState(10);
+        }
+    }
+}
